5c645e28a4ca6f9748a478d07a5b999d036f6d8d
Gram <eganeshram7@gmail.com>
2026-06-16 18:36:55 +0530

    update formula with release sha256

--- Formula/xchtml.rb
+++ Formula/xchtml.rb
@@ -4,7 +4,7 @@ class Xchtml < Formula
   desc "Generate beautiful HTML reports from Xcode .xcresult bundles"
   homepage "https://github.com/igram7/xchtml"
   url "https://github.com/igram7/xchtml/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "PLACEHOLDER_UPDATE_AFTER_RELEASE"
+  sha256 "290d8df35e2e42f355b3dcdccd97419ac6d5e50284402f16fbb2aeddc5de9ed2"
   license "MIT"
 
   depends_on "python@3.12"
